0d09461d5ef7305874dc6faa842967d822cd55ee
Venkastesh <venkatesh@ishaare.com>
2024-08-26 23:06:23 +0530

    trail1

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div style={{backgroundColor:"#ffcec1",width:"100%",height:"100vh",borderWidth:2}}>hey</div>
+    <div style={{backgroundColor:"#ffcec1",width:"100%",height:"100vh",borderWidth:2}}>
+      <img src='/—Pngtree—islamic chandelier_3413937.png' style={{position:"absolute",top:0,width:"100%",height:120,objectFit:"contain"}} />
+    </div>
   )
 }
 
