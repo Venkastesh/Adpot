6593abfff2c2232404749b91f1f2b268dfacd9f1
Venkastesh <venkatesh@ishaare.com>
2024-08-27 01:11:27 +0530

    changed to messging app

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
     const {pathname} = useLocation()
     return (
         <div className="flex flex-row items-center justify-between w-full p-5 shadow-[0_-15px_40px_-15px_rgba(0,0,0,0.3)]">
-            <div className="text-2xl font-medium capitalize transition-all">{pathname.replace("/","") || "chats"}</div>
+            <div className="text-2xl font-medium capitalize transition-all bg-gradient-to-r from-[#f9ce34] via-[#ee2a7b] to-[#6228d7]  bg-clip-text text-transparent">{pathname.replace("/","") || "chats"}</div>
             <SlOptionsVertical />
         </div>
     );
